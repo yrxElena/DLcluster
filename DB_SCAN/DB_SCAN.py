4d2c0986c74c6cdf64252a58daae55487a592bd7
怡然 余 <1121257919@qq.com>
2020-11-28 22:56:04 +0800

    将停止判别从固定的次数判定优化为方差判别，增加了模型的稳定性

--- DB_SCAN/DB_SCAN.py
+++ DB_SCAN/DB_SCAN.py
@@ -9,13 +9,19 @@ from sklearn import datasets
 import copy
 list_1 = []
 list_2 = []
+#aa=np.array([0,0,0,0,0,1])
+#vv=np.var(aa)
+#print(vv)
 
 def loadDataSet(path, dim):
     """
     加载所需的数据集
     path:数据集路径
     dim:数据维度
-    return：数据data, 距离矩阵Dis, dic_eps, dis_mean, Plist
+    return：数据data,
+    return：不同eps下每个点的领域点集合dic_eps
+    return：不同的Eps值dis_mean
+    return：不同Eps值下的MinPts值Plist
     """
     with open(path, 'r') as fd:
         raw_data = fd.readlines()
@@ -44,7 +50,7 @@ def loadDataSet(path, dim):
     Plist = [0 for i in range(count)]  # 存储不同Eps值下的MinPts
     keys_eps = dis_mean   # eps是一级索引
     subKeys_eps = [i for i in range(count)]   # 样本的序号是二级索引
-    dic_eps = dict(zip(keys_eps, [{} for i in range(count)]))
+    dic_eps = dict(zip(keys_eps, [{} for i in range(count)])) # 不同eps下每个点的邻域点集合
     for index in range(count):
         for i in range(count):
             value = []
@@ -54,21 +60,17 @@ def loadDataSet(path, dim):
                     Plist[index] += 1
             dic_eps[dis_mean[index]][i] = value
     Plist = np.divide(Plist, count);
-    return data, Dis, dic_eps, dis_mean, Plist
+    return data, dic_eps, dis_mean, Plist
  
-# 计算两个点之间的欧式距离，参数为两个元组
-#def dist(t1, t2):
-#    dis = math.sqrt((np.power((t1[0]-t2[0]),2) +
-#    np.power((t1[1]-t2[1]),2)))#power(x,y)函数，计算x的y次方
-#    # print("两点之间的距离为："+str(dis))
-#    return dis
 
 def DBscan(Dic, Eps, MinPts):
     """
+    DBSCAN算法的主要框架
     Dic:数据集字典
     Eps:邻域半径参数
     MinPts:制定邻域密度阈值（核心点阈值）
     """
+
     count = len(Eps)
     value_k = []
     C = [i for i in range(count)]
@@ -79,6 +81,7 @@ def DBscan(Dic, Eps, MinPts):
         visited = []
         C[index] = [-1 for i in range(count)]    # C为输出结果，默认是一个长度为num的值全为-1的列表
         k = -1     # 用k来标记不同的簇，k = -1表示噪声点
+        #C1 = -1 * np.ones((1,count))
         while len(UV) > 0:   # 如果还有没访问的点
             p = random.choice(UV)   # 随机选择一个unvisited对象;
             UV.remove(p)
@@ -91,7 +94,7 @@ def DBscan(Dic, Eps, MinPts):
                 k += 1
                 C[index][p] = k
                 CorePoints.append(p)   # 将核心点放入列表
-                for pi in unvisited:    # 对于p的epsilon邻域中的每个对象pi
+                for pi in unvisited:    # 对于p的eps邻域中的每个对象pi
                     if(pi in UV):
                         UV.remove(pi)
                         visited.append(pi)
@@ -100,20 +103,28 @@ def DBscan(Dic, Eps, MinPts):
                             unvisited = list(set(unvisited))                        
                     if C[index][pi] == -1:# 若pi不属于任何簇，C[pi] == -1说明C中第pi个值没有改动
                         C[index][pi] = k              
-            else:   # 如果p的epsilon邻域中的对象数小于指定阈值
+            else:   # 如果p的eps邻域中的对象数小于指定阈值,即为非核心点
                 C[index][p] = -1
         value_k.append(k)
-        if(index > 3):
-            if(value_k[-4] == value_k[-5] and value_k[-2] == value_k[-3] and value_k[-2] == value_k[-4] and value_k[-1] != value_k[-2]):
-                 return C[index - 1], Eps[index - 1], MinPts[index - 1]
-    #return value_k
+    value_k_np = np.array(value_k)
+    for index_i in range(len(value_k)):
+        if(index_i > 6):
+            #if(value_k[index_i - 4] == value_k[index_i - 5] and
+            #   value_k[index_i - 2] == value_k[index_i - 3] and 
+            #   value_k[index_i - 3] == value_k[index_i - 4] and 
+            #   value_k[index_i - 1] != value_k[index_i - 2]):
+            Var = np.var(value_k_np[index_i - 6 : index_i]) # 最近5个数的方差
+            if Var <= 0.139:
+                 return C[index_i], Eps[index_i], MinPts[index_i], value_k
+                 break
+
 
 if  __name__ == "__main__":
     #导入数据集
     data_file = u'dataset.csv'
     dim = 2   # 数据维度
-    dataSet, Dislist, DicEps, Epslist, Minptslist = loadDataSet(data_file, dim)   # count是指数据量
-    C, eps, minpts = DBscan(DicEps, Epslist, Minptslist)
+    dataSet, DicEps, Epslist, Minptslist = loadDataSet(data_file, dim)   # count是指数据量
+    C, eps, minpts,value_k = DBscan(DicEps, Epslist, Minptslist)
     print(C)
     x = []
     y = []
@@ -123,6 +134,7 @@ if  __name__ == "__main__":
     plt.figure(figsize=(8, 6), dpi=80)
     plt.scatter(x,y, c=C, marker='o')
     plt.show()
+
     x = []
     y = []
     for i in range(99):
@@ -131,8 +143,18 @@ if  __name__ == "__main__":
     plt.figure(figsize=(8, 6), dpi=80)
     plt.scatter(x,y, c=C, marker='o')
     plt.show()
-    print(x)
-    print(y)
+    #print(x)
+    #print(y)
+
+    x = []
+    y = []
+    for j in range(99):
+        x.append(j)
+        y.append(value_k[j])
+    plt.figure(figsize=(8, 6), dpi=80)
+    plt.scatter(x,y, c=C, marker='.')
+    plt.show()
+
 
 
 
